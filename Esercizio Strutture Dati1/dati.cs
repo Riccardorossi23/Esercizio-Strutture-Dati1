04318237a9ef0b13a07e0765245bcb48be45f8a8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:23:32 +0100

    Add project files.

---
+++ Esercizio Strutture Dati1/dati.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_Strutture_Dati1
+{
+    class dati
+    {
+        static void Main(string[] args)
+        {
+            List<double> numeri = new List<double>();
+            using (StreamReader sr = new StreamReader("fileDati.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    double f = Convert.ToDouble(line);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("copiafile.txt", true))
+            {
+                for (int i = numeri.Count - 1; i >= 0; i--)
+                {
+                    sw.WriteLine(numeri[i]);
+
+                }
+            }
+        }
+    }
+}
